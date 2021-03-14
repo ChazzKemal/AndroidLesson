0fed4254e59604b29b413167976199afe2c18cd6
ChazzKemal <alibaturkarakullukcu@gmail.com>
2021-03-14 20:59:39 +0300

    maps and login screen

--- app/src/main/java/com/example/secondlesson/MainActivity.kt
+++ app/src/main/java/com/example/secondlesson/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.secondlesson
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -14,7 +15,9 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Please enter both fields", Toast.LENGTH_SHORT).show()
             }
             else if(usernameInput.text.toString()=="yuabka" && passwordInput.text.toString()=="dragon" ){
-                Toast.makeText(this, "Your login is done", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "You will see the location of the person i am in love with", Toast.LENGTH_LONG).show()
+                intent= Intent(this,MapsActivity::class.java)
+                startActivity(intent)
             }
             else{
                 Toast.makeText(this, "Try Again", Toast.LENGTH_SHORT).show()
